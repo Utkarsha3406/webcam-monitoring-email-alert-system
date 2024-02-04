c326d2f1011945f62fcdfd211ca6e61fbec2e117
Utkarsha3406 <up3406@srmist.edu.in>
2024-02-05 00:45:48 +0530

    added object detection code

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import cv2
 import time
-
+from emailing import send_email
 video = cv2.VideoCapture(0)
 time.sleep(1)
+
+first_frame = None
+status_list = []
+
 while True:
-
+    status = 0
     check,frame = video.read()
-    cv2.imshow("My video", frame)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grey_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
 
+    if first_frame is None:
+        first_frame = grey_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+    print(status_list)
+    cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
