0afb0c342af0890ba512f58eb2b045a2f47b2f81
Utkarsha3406 <up3406@srmist.edu.in>
2024-02-05 22:38:35 +0530

    extract images from webcam video

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import time
 from emailing import send_email
+import glob
+
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
@@ -31,6 +34,12 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
