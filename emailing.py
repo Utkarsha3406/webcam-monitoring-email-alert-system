6733c2d36c5d00633a4bc8218bbfeb6886577744
Utkarsha3406 <up3406@srmist.edu.in>
2024-02-05 23:22:08 +0530

    send email with attachment

--- emailing.py
+++ emailing.py
@@ -1,2 +1,29 @@
-def send_email():
-    print("Email is sent")
+import smtplib
+from PIL import Image
+from io import BytesIO
+from email.message import EmailMessage
+PASSWORD = "__" # your 16 alphabet app password after turning on 2 step verification on your gmail
+SENDER = "__" #your email address
+RECEIVER = "app8falsk@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    image_data = BytesIO(content)
+    img = Image.open(image_data)
+    image_format = img.format
+    email_message.add_attachment(content, maintype="image", subtype=image_format)
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
